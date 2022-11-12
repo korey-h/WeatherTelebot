ea6f73775362d04bd55bd3983c36aaf9df76f6f3
Егор Уфаев <eufaev@yandex.ru>
2022-11-13 01:00:22 +0500

    tune commands stack, tune table view

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from telebot import TeleBot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import (
+    ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton,
+    InlineKeyboardMarkup)
 
 from datetime import datetime
 import os
@@ -21,11 +23,30 @@ def get_user(message) -> User:
     return users.setdefault(user_id, User(id=user_id))
 
 
+def try_exec_stack(user: User):
+    command = user.cmd_stack_pop()
+    if command and callable(command[0]):
+        command[0](command[1])
+    else:
+        user.cmd_stack = command
+
+
 def make_keyboard(lang='ru'):
     keyboard = ReplyKeyboardMarkup(row_width=4, resize_keyboard=True)
     but_town = KeyboardButton(MESSAGES[lang]['but_town'])
     but_year_ago = KeyboardButton(MESSAGES[lang]['but_year_ago'])
     keyboard.add(but_town, but_year_ago)
+    return keyboard
+
+
+def make_inline_keys(lang='ru'):
+    keyboard = InlineKeyboardMarkup()
+    but_cancel = InlineKeyboardButton(
+        text=MESSAGES[lang]['cancel'],
+        callback_data=MESSAGES[lang]['cancel']
+    )
+
+    keyboard.add(but_cancel, )
     return keyboard
 
 
@@ -40,42 +61,67 @@ def welcome(message):
 
 
 @bot.message_handler(commands=['город'])
-def settown(message):
-    user = get_user(message)
-    user.last_req = message.text
+def settown(message, user=None, command=None):
+    if not user:
+        user = get_user(message)
+    if not command:
+        command = message.text
+    user.cmd_stack = command
     bot.send_message(user.id, MESSAGES[user.lang]['settown'])
+
+
+@bot.callback_query_handler(func=lambda call: True, )
+def cancel_comm(call):
+    user = get_user(call.message)
+    if call.data == MESSAGES[user.lang]['cancel']:
+        command = user.cmd_stack_pop()
+        bot.send_message(
+            user.id,
+            MESSAGES[user.lang]['cancel_mess'].format(command[0]))
 
 
 @bot.message_handler(commands=['год_назад'])
 def get_year_ago(message):
     user = get_user(message)
-    user.last_req = message.text
     if user.town:
         now = datetime.now()
         day = int(now.strftime('%d'))
         month = int(now.strftime('%m'))
         year = int(now.strftime('%Y')) - 1
-        stat = MonthStat(town_id=user.town, year=year, month=month)
+        stat = MonthStat(town_id=user.town, year=year,
+                         month=month, town_name=user.town_name)
         data = stat.daystat(day, pretty=True, as_pic=True)
         if data:
-            # bot.send_message(user.id, f'<pre>{data}</pre>', parse_mode='html')
-            bot.send_photo(user.id, photo=data, parse_mode='html')
+            bot.send_photo(user.id, photo=data)
         else:
             bot.send_message(user.id, MESSAGES[user.lang]['no_data'])
+    else:
+        user.cmd_stack = (get_year_ago, message)
+        settown(message, user, MESSAGES[user.lang]['but_town'])
 
 
 @bot.message_handler(content_types=["text"])
 def auditor(message):
     user = get_user(message)
-    last_command = user.last_req
-    if last_command == '/город':
+    last_command = user.cmd_stack_pop()
+    if last_command and last_command[0] == '/город':
+        kwargs = {}
         town_id = towns.get_id(message.text)
         if town_id:
             user.town = town_id
+            user.town_name = message.text.capitalize()
             mess = MESSAGES[user.lang]['town_finded']
+            bot.send_message(user.id, mess, **kwargs)
+            bot.send_chat_action(user.id, 'typing', 10)
+            try_exec_stack(user)
+
         else:
             mess = MESSAGES[user.lang]['town_nothing']
-        bot.send_message(user.id, mess)
+            user.cmd_stack = last_command
+            kwargs = {'reply_markup': make_inline_keys(lang=user.lang)}
+            bot.send_message(user.id, mess, **kwargs)
+    else:
+        user.cmd_stack = last_command
 
 
 bot.polling(non_stop=True)
--- models.py
+++ models.py
@@ -1,15 +1,23 @@
 class User:
-    def __init__(self, id, lang='ru', town=None):
+    def __init__(self, id, lang='ru', town=None, town_name=None):
         self.id = id
         self.lang = lang
         self.town = town
+        self.town_name = town_name
         self._commands = []
 
-    def get_last_req(self):
+    def get_cmd_stack(self):
+        if len(self._commands) > 0:
+            return self._commands[-1]
+
+    def set_cmd_stack(self, cmd_stack):
+        if not isinstance(cmd_stack, (list, tuple)):
+            self._commands.append((cmd_stack, ))
+        else:
+            self._commands.append(cmd_stack)
+
+    cmd_stack = property(get_cmd_stack, set_cmd_stack)
+
+    def cmd_stack_pop(self):
         if len(self._commands) > 0:
             return self._commands.pop()
-
-    def set_last_req(self, last_req):
-        self._commands.append(last_req)
-
-    last_req = property(get_last_req, set_last_req)
